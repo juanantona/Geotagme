bcc4790865f5cbcaa81d6f51e9f6b0b50f8ae676
Juan Antona <juan.antonadelval@gmail.com>
2015-05-19 12:55:39 +0200

    setup activerecord-postgis-adapter

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails"
+require "rails/all"
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
+
 
 # require "rails/test_unit/railtie"
 
@@ -31,6 +25,6 @@ module Mock3
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
