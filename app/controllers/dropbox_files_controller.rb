ea7a7337d3d369ba80d6b6453aa6838efbf12f3e
Juan Antona <juan.antonadelval@gmail.com>
2015-05-20 14:07:13 +0200

    clean code

--- app/controllers/dropbox_files_controller.rb
+++ app/controllers/dropbox_files_controller.rb
@@ -1,56 +1,16 @@
 class DropboxFilesController < ApplicationController
   
   def dashboard
-
-      @client = dropbox_client
-  
-      #download_photos_and_save_db_record()
       
-      render 'dashboard'
+    render 'dashboard'
         
   end
 
   def view_photos
 
     @photos = DropboxFile.where(supplier_id: current_user.supplier_id)
-    
-  end
 
-  def viewmap
-
-      @client = dropbox_client
-
-      photo = @client.find('/photos/IMAG0394.jpg')
-      destination_file_full_path = Rails.root.to_s + "/" + photo.path.to_s.split("/").last
-            
-      remote_photo = open(photo.direct_url.url,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read
-      # exif_data = EXIFR::JPEG.new(remote_photo)
-
-      if File.exists?(destination_file_full_path)
-      else
-        open(destination_file_full_path, 'wb') do |file|
-            file << photo.download
-        end
-      end
-
-      if File.exists?(destination_file_full_path)
-         dp_file = File.open(destination_file_full_path)
-      end
-
-      if dp_file
-        record = DropboxFile.new(:image => dp_file)
-        record.url = photo.direct_url.url
-        record.save
-      end
-     
-      exif_data = EXIFR::JPEG.new(destination_file_full_path)
-
-     # binding.pry
-
-      @latitude = exif_data.gps.latitude
-      @longitude = exif_data.gps.longitude
-
-      render "viewmap"
+    download_photos_and_save_db_record()
     
   end
 
@@ -68,12 +28,19 @@ class DropboxFilesController < ApplicationController
          unless dropbox_element.is_dir?
                    
            destination_file_full_path = Rails.root.to_s + "/" + dropbox_element.path.to_s.split("/").last
+           
            unless DropboxFile.where(url: dropbox_element.direct_url.url).where(supplier_id: current_user.supplier_id).exists?
              
-             open(destination_file_full_path, 'wb') do |file|
-               file << dropbox_element.download
-             end
+             # remote_photo = open(photo.direct_url.url,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read
              
+             begin
+                 open(destination_file_full_path, 'wb') do |file|
+                   file << dropbox_element.download
+                 end
+             rescue
+                 puts "Exception occured while downloading..."
+             end
+
              save_db_record(dropbox_element, destination_file_full_path)
                         
            end
@@ -93,38 +60,14 @@ class DropboxFilesController < ApplicationController
      
      exif_data = EXIFR::JPEG.new(destination_file_full_path)
      # record.geolocation = 'POINT(#{exif_data.gps.longitude} #{exif_data.gps.latitude})'
-     record.photo_timestamps = exif_data.date_time
+     record.photo_timestamps = exif_data.exif[:date_time_digitized]
       
      record.save
+
+     # File.delete(local_file)
   
   end
 
-
- 
-  def directory_hash(obj)
-      if obj.is_dir?
-        list_hash(obj)
-      else
-        return obj
-        # download_and_create(obj)
-      end
-  end
-  
-  def list_hash(obj)
-      obj.ls.each do |s|
-        directory_hash(s)
-      end
-  end
-  
-  def download_and_create(obj)
-      path = download_file_from_url(obj)
-      dp_file = File.open(path) if File.exists?(path)
-      if dp_file
-        DropboxFile.create(:image => dp_file)
-        File.delete(dp_file)
-      end
-  end
-  
   def download_file_from_url(obj)
       destination_file_full_path = Rails.root.to_s + "/" + obj.path.to_s.split("/").last
       begin
@@ -136,7 +79,7 @@ class DropboxFilesController < ApplicationController
       rescue
         puts "Exception occured while downloading..."
       end
-      destination_file_full_path
+      
   end
 
 end
