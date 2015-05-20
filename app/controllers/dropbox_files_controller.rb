6a29783ae61b37229d521a5844a067d71d4eecdf
Juan Antona <juan.antonadelval@gmail.com>
2015-05-20 10:30:01 +0200

    method to download photos operative

--- app/controllers/dropbox_files_controller.rb
+++ app/controllers/dropbox_files_controller.rb
@@ -4,6 +4,10 @@ class DropboxFilesController < ApplicationController
 
       @client = dropbox_client
   
+      download_photos_and_save_db_record()
+
+      binding.pry
+
       @photos=[]  
 
       if @client.ls.any?
@@ -12,43 +16,7 @@ class DropboxFilesController < ApplicationController
            end
        end
 
-
-       # if @client.ls.any?
-       #     @client.ls.each do |f|
-       #       unless f.is_dir?
-               
-       #         destination_file_full_path = Rails.root.to_s + "/" + f.path.to_s.split("/").last
-               
-       #         unless File.exists?(destination_file_full_path)
-                 
-       #           open(destination_file_full_path, 'wb') do |file|
-       #             file << f.download
-       #           end
-
-       #           dp_file = File.open(destination_file_full_path)
-       #           record = DropboxFile.new(:image => dp_file)
-       #           record.supplier_id = current_user.id if (current_user.role == "supplier")
-       #           record.url = f.direct_url.url
-       #           record.path = f.path
-                 
-       #           exif_data = EXIFR::JPEG.new(destination_file_full_path)
-       #           # record.geolocation = 'POINT(#{exif_data.gps.longitude} #{exif_data.gps.latitude})'
-       #           record.timestamps = exif_data.date_time
-       #           binding.pry 
-       #           record.save
-                 
-       #         end
-
-       #       end
-
-       #    end
-    
-      #end
-      
-
-
-
-      render 'welcome'
+       render 'welcome'
         
   end
 
@@ -91,6 +59,55 @@ class DropboxFilesController < ApplicationController
   end
 
   private
+
+  def download_photos_and_save_db_record()
+
+    @client = dropbox_client
+    @photo_folder = "/photos"
+    
+    if @client.ls(@photo_folder).any?
+
+           @client.ls(@photo_folder).each do |f|
+             
+             
+             unless f.is_dir?
+              
+
+               
+               
+               destination_file_full_path = Rails.root.to_s + "/" + f.path.to_s.split("/").last
+               
+               unless File.exists?(destination_file_full_path)
+                 
+                 open(destination_file_full_path, 'wb') do |file|
+                   file << f.download
+                 end
+
+                 dp_file = File.open(destination_file_full_path)
+                 record = DropboxFile.new(:image => dp_file)
+                 record.supplier_id = current_user.id if (current_user.role == "supplier")
+                 record.url = f.direct_url.url
+                 record.path = f.path
+                 
+                 exif_data = EXIFR::JPEG.new(destination_file_full_path)
+                 # record.geolocation = 'POINT(#{exif_data.gps.longitude} #{exif_data.gps.latitude})'
+                 record.photo_timestamps = exif_data.date_time
+                 binding.pry 
+                 record.save
+                 
+               end
+
+             end
+
+          end
+    
+      end
+
+  end
+
+
+
+
  
   def directory_hash(obj)
       if obj.is_dir?
