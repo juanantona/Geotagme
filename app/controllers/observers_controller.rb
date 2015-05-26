7a56b4cc267068fb2e1df7c7c7ae7e63d82c1a68
Juan Antona <juan.antonadelval@gmail.com>
2015-05-26 11:26:31 +0200

    save observer record

--- app/controllers/observers_controller.rb
+++ app/controllers/observers_controller.rb
@@ -8,10 +8,34 @@ class ObserversController < ApplicationController
 
 	def create
 
-		
+		@observer = Observer.new(observer_params)
+		@observer.user_id = current_user.id
+
+		if @observer.save
+			redirect_to dashboard_path
+		else
+			redirect_to observer_path
+		end	
 		
 	end
 
+	def has_secure_password 
+		
+	end
 
+	private
+
+	def observer_params
+
+		params.require(:observer)
+		  .permit(:name, 
+		   :email,
+		   :password,
+		   :password_confirmation,
+		   :start_temp_permissions,
+		   :terminate_temp_permissions,
+		   :user_id, 
+		  )	
+	end
 
 end
