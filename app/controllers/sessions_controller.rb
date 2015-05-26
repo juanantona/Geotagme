76e19848528c64c1bd09fee0e9c468e6af53aa05
Juan Antona <juan.antonadelval@gmail.com>
2015-05-26 21:04:08 +0200

    login as observer operative

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,16 @@
 class SessionsController < ApplicationController
 
-	# renders the login form
 	def new
+		@observer = Observer.new
 		render 'new'
 	end
 
-	# used to log a user in
 	def create
 		
-		@user = User.find_by(email: params[:email])
+		@observer = Observer.find_by(email: params[:observer][:email])
 		
-		if (@user && @user.authenticate(params[:password]))
-			session[:user_id] = @user.id
+		if (@user && @user.authenticate(params[:observer][:password]))
+			session[:user_id] = observer.id
 				        			
 			redirect_to dashboard_path
 
@@ -25,7 +24,7 @@ class SessionsController < ApplicationController
 	def destroy
 		session[:user_id] = nil
 			    
-		redirect_to root_path 
+		redirect_to login_path 
 		
 	end
 end
