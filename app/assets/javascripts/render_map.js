202b2c6f04a0b929fbfb7c6cb00f73261c8b6a4a
Juan Antona <juan.antonadelval@gmail.com>
2015-05-28 17:33:40 +0200

    hide menu observers if role is photogrpher

--- app/assets/javascripts/render_map.js
+++ app/assets/javascripts/render_map.js
@@ -3,18 +3,18 @@
      osm = L.tileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib});
 
  var arrayOfLatLngs = [];
-              arrayOfLatLngs.push(new L.LatLng(30.74656, -5.33936));
-              arrayOfLatLngs.push(new L.LatLng(29.95018, -6.78406));
-              arrayOfLatLngs.push(new L.LatLng(30.47472, -5.42175));
-              arrayOfLatLngs.push(new L.LatLng(42.71473, 1.71387));
+        arrayOfLatLngs.push(new L.LatLng(30.74656, -5.33936));
+        arrayOfLatLngs.push(new L.LatLng(29.95018, -6.78406));
+        arrayOfLatLngs.push(new L.LatLng(30.47472, -5.42175));
+        arrayOfLatLngs.push(new L.LatLng(42.71473, 1.71387));
 
  var bounds = new L.LatLngBounds(arrayOfLatLngs);    
 
  var map = new L.Map('mymap', 
  {
-      center: bounds.getCenter(),
-      zoom: 5,
-      layers: [osm],
+        center: bounds.getCenter(),
+        zoom: 5,
+        layers: [osm],
  });
 
 map.fitBounds(bounds);
@@ -22,15 +22,16 @@ map.fitBounds(bounds);
 var popup = L.popup();
 
 function showPopup(coord){
-  var lat =  Number(coord.split(',')[0]);
-  var lon =  Number(coord.split(',')[1]);
-  var latlon = new L.LatLng(lat, lon);
-  popup
-  .setLatLng(latlon)
-  .setContent("This photo was taken here")
-  .openOn(map);
-  map.setView(latlon)
+        var lat =  Number(coord.split(',')[0]);
+        var lon =  Number(coord.split(',')[1]);
+        var latlon = new L.LatLng(lat, lon);
+        popup
+        .setLatLng(latlon)
+        .setContent("This photo was taken here")
+        .openOn(map);
+        map.setView(latlon)
 };
+   
 
 
 
